1909395e5ce0569213d997be6fb445335bbca481
MelvinPeepers <peepersm@gmail.com>
2019-09-06 11:15:58 -0400

    reagrranged a bunch of things and made alot of changes. Really messed up my commits. Turned out I typed const bcryptjs = instead of const bcrypt = required('bcryptjs') and that was causing my login not to work

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,3 +1,7 @@
+const bcrypt = require('bcryptjs');
+
+const Users = require('../users/users-model.js');
+
 module.exports = function restricted(req, res, next) {
     const { username, password } = req.headers;
 
